47c1bba860196ddc6ab300dee542f120ec3e7a0e
Glen Dudek <glen@twyxter.com>
2013-08-16 10:53:06 -0400

    Remove directive to create database.yml as user root. This fails when executing via vagrant on an Ubuntu (11.10) development system as chef tries to execute a 'chown' on the NFS client, which is not permitted by the Ubuntu NFS server even though all client uids (including root) are mapped to the same uid on the server, alas. I am guessing the Max OS X NFS server allows the chown, as no owner uids will be changed by the chown.

--- cookbooks/rails_development/recipes/mysql.rb
+++ cookbooks/rails_development/recipes/mysql.rb
@@ -1,7 +1,6 @@
 # Create mysql database.yml - should not be checked in.
 # Database names will be vagrant_#{ENV}.
 template '/vagrant/config/database.yml' do
-  user 'root'
   source 'mysql/database.yml.erb'
   variables({
     :password => node['mysql']['server_root_password']
--- cookbooks/rails_development/recipes/postgres.rb
+++ cookbooks/rails_development/recipes/postgres.rb
@@ -1,7 +1,6 @@
 # Create postgres database.yml - should not be checked in.
 # Database names will be vagrant_#{ENV}.
 template '/vagrant/config/database.yml' do
-  user 'root'
   source 'postgres/database.yml.erb'
   variables({
     :password => node['postgresql']['password']['postgres']
